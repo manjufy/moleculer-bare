f011e1f60f6fccb739fa43ee67e18f8f7c7c73a4
Manjunath Reddy <manju@Manjunaths-MacBook-Pro.local>
2020-06-09 17:14:22 +0800

    Git ignore
     Author:    Manjunath Reddy <manju16832003@gmail.com>
    
     Author:    Manjunath Reddy <manju16832003@gmail.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+# Moleculer Barebone Implementation
+
+Repo implements barebone moleculer implementation that consists for 
+
+- An API Gateway
+- A internal service
+
+## How to run
+
+Moleculer suggests to use default transporter, since we need to download and install NAT server,  I have decided to use Redis. Make sure to have redis installed before running the services
+
+```
+git clone 
+cd > moleculer-bare
+cd > moleculer-bare > node index.js
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+const { ServiceBroker } = require('moleculer');
+const HTTPServer = require("moleculer-web");
+
+// Create the broker for node-2 as Product Service and define the NodeId and set the communciation bus
+const brokerProductNode = new ServiceBroker({
+    nodeID: "product-node",
+    transporter: "redis://localhost:6379"
+});
+
+// Create the product service
+brokerProductNode.createService({
+    name:  "products",
+    actions: {
+        // Define service action that returns the available products
+        listProducts(ctx) {
+          return [
+            { name: "Apples", price: 5 },
+            { name: "Oranges", price: 3 },
+            { name: "Bananas", price: 2 }
+          ];
+        }
+      }
+});
+
+// Create the broker for node-1 as API Gateway Service and define nodeID and set the communication bus
+const brokerAPIGatewayNode = new ServiceBroker({
+    nodeID: "api-gateway-node",
+    transporter: "redis://localhost:6379"
+});
+
+// Create the gateway service
+brokerAPIGatewayNode.createService({
+    name: 'api', // service name
+    mixins: [HTTPServer], // http server
+    settings: {
+        port: 4000,
+        routes: [
+            {
+                aliases: {
+                    // When the "GET /products" request is made the "listProducts" action of "products" service is executed
+                    "GET /products": "products.listProducts"
+                }
+            }
+        ],
+    }
+});
+
+// Start both brokers
+Promise.all([
+    brokerAPIGatewayNode.start(),
+    brokerProductNode.start()
+]);
